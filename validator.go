7c6db4bc43557d6cc1b58e9b6164446c98c330a0
Alberto Morgante <alknopfler@gmail.com>
2021-07-07 12:48:10 +0200

    refactoring to a lib

--- validator.go
+++ validator.go
@@ -36,15 +36,11 @@ func Validate(input []byte) WebData {
 		auth := string(sDec)
 
 		err, res := loginToRegistry(k, auth)
-		fmt.Println("xxxxxxx")
 		if err != nil || res == RES_CONERROR {
 			resultKOConArray = append(resultKOConArray, k+"\n"...)
-			fmt.Println("KOCON-" + k + "\n")
 		} else if res == RES_VALID {
-			fmt.Println("OK-" + k + "\n")
 			resultOKArray = append(resultOKArray, k+"\n"...)
 		} else if res == RES_EXPIRED {
-			fmt.Println("KOexpired-" + k + "\n")
 			resultKOArray = append(resultKOArray, k+"\n"...)
 		}
 	}
@@ -77,20 +73,5 @@ func loginToRegistry(url, auth string) (error, string) {
 
 	defer resp.Body.Close()
 	return nil, ""
-	/*
 
-		if err2 != nil {
-			return err2, RES_CONERROR
-		} else if resp.StatusCode == http.StatusOK || resp.StatusCode == http.StatusAccepted {
-			return nil, RES_VALID
-		} else if resp.StatusCode == http.StatusNotFound {
-			return nil, RES_CONERROR
-		} else if resp.StatusCode == http.StatusUnauthorized || resp.StatusCode == http.StatusForbidden {
-			return nil, RES_EXPIRED
-		} else {
-			return err, RES_CONERROR
-		}
-		defer resp.Body.Close()
-
-	*/
 }
